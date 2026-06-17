36033583db3fe9ce2b3aa7a23446b7ee5adf216d
Arvind Kumar <24cd3008@rgipt.ac.in>
2026-06-18 02:47:30 +0530

    Remove Firebase configuration from firebase_options.dart
    Removed hardcoded Firebase options for Android and iOS.

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -37,21 +37,5 @@ class DefaultFirebaseOptions {
     }
   }
 
-  // REPLACE THESE VALUES WITH YOUR OWN FROM google-services.json
-  static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyCmopa3nYv2Gs9y15DJLfIEzGKcsxLR3VI',              // ← From google-services.json
-    appId:  "1:225843432203:android:e220f12c42ff51161e700e",                // ← From google-services.json
-    messagingSenderId: '225843432203', // ← From google-services.json
-    projectId: 'expense-tracker-c2fdf',        // ← From google-services.json
-    storageBucket: "expense-tracker-c2fdf.firebasestorage.app", // ← From google-services.json
-  );
-
-  static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: 'AIzaSyCmopa3nYv2Gs9y15DJLfIEzGKcsxLR3VI',              // ← From google-services.json
-    appId:  "1:225843432203:android:e220f12c42ff51161e700e",                // ← From google-services.json
-    messagingSenderId: '225843432203', // ← From google-services.json
-    projectId: 'expense-tracker-c2fdf',        // ← From google-services.json
-    storageBucket: "expense-tracker-c2fdf.firebasestorage.app",
-    iosBundleId: 'com.example.advanceExpanseTrackerApp',
-  );
+ 
 }
